7ff4f3cbcf5d4fea5df99368f4d424696d5897d7
Michal Dabrowski <dabrowski@brillante.pl>
2018-04-10 09:24:10 +0200

    Removing old pages

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.pic a').lightBox({
-		imageLoading: 'images/gallery/loading.gif',
-		imageBtnClose: 'images/gallery/close.gif',
-		imageBtnPrev: 'images/gallery/prev.gif',
-		imageBtnNext: 'images/gallery/next.gif'
-	});
-});
